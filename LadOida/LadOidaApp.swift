aac668807ff5a2703df98efd068d099ec2482303
Ing. Ebu Celik <ebucelik1@hotmail.com>
2024-06-09 17:31:36 +0200

    initial setup

---
+++ LadOida/LadOidaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LadOidaApp.swift
+//  LadOida
+//
+//  Created by Ing. Ebu Celik, BSc on 09.06.24.
+//
+
+import SwiftUI
+
+@main
+struct LadOidaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
